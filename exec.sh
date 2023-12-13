30cabdb7bdb4fce7d8e1bb71e3d1f43569f59076
riazi-r <68187749+riazi-r@users.noreply.github.com>
2023-12-13 06:13:02 +0330

    Add files via upload

---
+++ exec.sh
@@ -0,0 +1,19 @@
+gmx grompp -f min.mdp -c boxedsoln.gro -p topol.top -r boxedsoln.gro -o em.tpr &&
+gmx mdrun -deffnm em &&
+
+gmx grompp -f nvt.mdp 1-c em.gro -p topol.top -r em.gro -o nvt.tpr &&
+gmx mdrun -deffnm nvt -update gpu -nb gpu -pme gpu -bonded gpu && sleep 2 &&
+
+gmx grompp -f npt.mdp -c nvt.gro -p topol.top -r nvt.gro -t nvt.cpt -o npt.tpr &&
+gmx mdrun -deffnm npt -update gpu -nb gpu -pme gpu -bonded gpu && sleep 3 &&
+
+gmx grompp -f awh.mdp -c npt.gro -p topol.top -r npt.gro -n awh.ndx -t npt.cpt -o awh.tpr &&
+
+for N in {0..10}
+
+do
+echo $N
+;gmx mdrun -deffnm awh -update gpu -nb gpu -pme gpu -pf pullf.xvg -px pullx.xvg -cpi awh.cpt || sleep 6
+gmx mdrun -s awh-ext.tpr -deffnm awh -cpi awh.cpt -px pullx.xvg -pf pullf.xvg || sleep 6
+done 
+  
