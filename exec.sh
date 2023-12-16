c09df09a2eedf78fc332c2bf18c35ff5c00fc73a
riazi-r <68187749+riazi-r@users.noreply.github.com>
2023-12-16 04:11:30 +0330

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -17,3 +17,5 @@ gmx mdrun -deffnm awh -update gpu -nb gpu -pme gpu -pf pullf.xvg -px pullx.xvg -
 gmx mdrun -s awh-ext.tpr -deffnm awh -cpi awh.cpt -px pullx.xvg -pf pullf.xvg || sleep 6
 done 
   
+#gmx grompp -f md_pull.mdp -c npt.gro -p topol.top -r npt.gro -n index.ndx -t npt.cpt -o pull.tpr
+#gmx mdrun -deffnm pull -pf pullf.xvg -px pullx.xvg
