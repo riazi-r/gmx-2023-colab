efcd96b95fef0b42af110fb03338055167431905
riazi-r <68187749+riazi-r@users.noreply.github.com>
2023-12-16 14:48:51 +0330

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,12 +1,21 @@
-echo 0 | gmx trjconv -s npt.tpr -f npt.xtc -o npt.gro -sep -skip 3
+gmx grompp -f min.mdp -c boxedsoln.gro -p topol.top -r boxedsoln.gro -o em.tpr &&
+gmx mdrun -deffnm em &&
 
-for i in {0..16}
+gmx grompp -f nvt.mdp -c em.gro -p topol.top -r em.gro -o nvt.tpr &&
+gmx mdrun -deffnm nvt -update gpu -nb gpu -pme gpu -bonded gpu && sleep 2 &&
+
+gmx grompp -f npt.mdp -c nvt.gro -p topol.top -r nvt.gro -t nvt.cpt -o npt.tpr &&
+gmx mdrun -deffnm npt -update gpu -nb gpu -pme gpu -bonded gpu && sleep 3 &&
+
+gmx grompp -f awh.mdp -c npt.gro -p topol.top -r npt.gro -n awh.ndx -t npt.cpt -o awh.tpr &&
+
+for N in {0..10}
+
 do
-gmx grompp -f smd.mdp -c npt$i.gro -p topol.top -r npt$i.gro -n awh.ndx -o smd$i.tpr
-
-echo $i
-
-gmx mdrun -deffnm smd$i -pf pullf$i.xvg -px pullx$i.xvg -cpi smd$i.cpt -update gpu
- done 
-
- 
+echo $N
+gmx mdrun -deffnm awh -update gpu -nb gpu -pme gpu -pf pullf.xvg -px pullx.xvg -cpi awh.cpt || sleep 6
+#gmx mdrun -s awh-ext.tpr -deffnm awh -cpi awh.cpt -px pullx.xvg -pf pullf.xvg || sleep 6
+done 
+  
+#gmx grompp -f smd.mdp -c npt.gro -p topol.top -r npt.gro -n awh.ndx -t npt.cpt -o smd.tpr
+#gmx mdrun -deffnm smd -pf pullf.xvg -px pullx.xvg
