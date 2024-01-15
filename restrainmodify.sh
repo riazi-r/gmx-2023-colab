0964c36e9e96b4b9bc7dabb033460b0fc271214f
riazi-r <68187749+riazi-r@users.noreply.github.com>
2024-01-15 04:05:26 +0330

    Update restrainmodify.sh

--- restrainmodify.sh
+++ restrainmodify.sh
@@ -1,15 +1,30 @@
-cd toppar
-cp PROM.itp PROM_ORIGINAL.itp
-restrain=$(awk '/ifdef POSRES_left/{print NR}' PROM.itp)
-first=$(($restrain+2))
-last=$(awk '$0~"POSRES_FC_BB"{n=NR}END{print n}' PROM.itp)
-#$(awk 'END /POSRES_FC_BB/{print NR}' PROM.itp)
-#test=$(($first+6))
-for (( l=$first; l<=$last; l+=1 ))
-do
-atom=$(awk -v "l=$l" 'NR==l {print $1}' PROM.itp)
-k=$(awk -v "l=$l" 'NR==l {print $3}' PROM.itp)
-sed -i ''$l'i '$atom'     2    1    1    '$k'' PROM.itp
-next=$(($l+1))
-sed -i ''$next'd' PROM.itp
-done
+destination=`pwd`
+define_left=$(awk '/set left/ {print $0}'  centerfinder.tcl)
+  leftproteins=$(echo $define_left | grep -o -P '(?<=segid).*?(?=serial)'|tr -d 'to','or')
+  leftchain_No=$(echo $leftproteins | awk '{print NF}')  
+  for (( chain=1; chain<=$leftchain_No; chain+=1 ))
+  do
+   segid=$(echo $leftproteins | awk '{print $chain}')
+   cp $destination/toppar/${segid}.itp $destination/toppar/${segid}_original.itp
+   sed -i 's/ifdef POSRES/ifdef POSRES_left/' $destination/toppar/${segid}.itp
+   posres=$(awk '/ifdef POSRES_left/{print NR}' $destination/toppar/${segid}.itp)
+   first=$(($posres+2))
+   last=$(awk '$0~"POSRES_FC"{n=NR}END{print n}' $destination/toppar/${segid}.itp)
+   i=0
+   j=1
+	mv $destination/toppar/${segid}.itp  $destination/toppar/${segid}_${i}.itp
+    for (( line=$first; line<=$last; line+=1 ))
+    do
+     atom=$(awk -v "a=$line" 'NR==a {print $1}' $destination/toppar/${segid}_${i}.itp)
+     k=$(awk -v "a=$line" 'NR==a {print $3}' $destination/toppar/${segid}_${i}.itp)
+	 awk -v "a=$line" 'NR==a { print "'$atom'     2    1    1    '$k'" ; next } 1' $destination/toppar/${segid}_${i}.itp > $destination/toppar/${segid}_${j}.itp
+	 rm  $destination/toppar/${segid}_${i}.itp
+	 ((i++))
+	 ((j++))
+     
+     #sed -i ''$line'i '$atom'     2    1    1    '$k'' $destination/toppar/${segid}.itp
+     #next=$(($line+1))
+     #sed -i ''$next'd' $destination/toppar/${segid}.itp
+    done
+  done	 
+  
