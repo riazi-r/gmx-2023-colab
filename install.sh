4ba894c691a472cc55bae0b8cefb1df542829b11
riazi-r <68187749+riazi-r@users.noreply.github.com>
2024-02-19 11:00:16 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-tar xfz gromacs-2023.3.tar.gz &&
-cd gromacs-2022
+wget https://ftp.gromacs.org/gromacs/gromacs-2024.tar.gz
+tar xfz gromacs-2024.tar.gz &&
+cd gromacs-2024
 mkdir build
 cd build
-cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DCMAKE_INSTALL_PREFIX=/usr/local/gromacs2022 -DCMAKE_BUILD_TYPE=Debug -DGMX_GPU=CUDA -DGMXAPI=ON &&
-make -j 10 && 
+cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DCMAKE_INSTALL_PREFIX=/usr/local/gromacs2024 -DCMAKE_BUILD_TYPE=Debug -DGMX_GPU=CUDA -DGMXAPI=ON &&
+make -j 8 && 
 make check &&
 sudo make install &&
-source /usr/local/gromacs2022/bin/GMXRC
+source /usr/local/gromacs2024/bin/GMXRC
 
 #-DGMX_CUDA_TARGET_SM=90
 #-DBUILD_SHARED_LIBS=ON
